87678b5575f83f7064fc45b3e9826cfc07efe831
SOUMYA GHOSH <subhathinks@gmail.com>
2023-07-22 21:24:25 +0530

    First Commit

---
+++ next-app/app/page.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+       <section className="flex-start flex-col paddings mb-16">
+            <h5>Categories</h5>
+            <h5>Posts</h5>
+            <h5>LoadMore</h5>
+       </section>
+    )
+}
+
+export default Home
